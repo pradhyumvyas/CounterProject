023916b66da2659f865eb7a32e9c7cfaffe34c0f
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-08-12 11:31:12 +0530

    Adding JS and setInterval Function

---
+++ script.js
@@ -0,0 +1,11 @@
+var counter = document.querySelector(".counter"); 
+var follower = document.querySelector(".follower"); 
+
+let count = 1;
+
+setInterval(()=>{
+    if(count<1000){
+        count++;
+        counter.innerText = count;
+    }
+},1)
